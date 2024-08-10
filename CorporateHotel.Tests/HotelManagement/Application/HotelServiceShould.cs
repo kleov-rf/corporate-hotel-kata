edcc8f3c7a727aed3d7bd60c76366c14fe51efea
D-Arturo <arturomf88@gmail.com>
2024-08-10 18:50:00 +0200

    test: return found hotel from service

--- CorporateHotel.Tests/HotelManagement/Application/HotelServiceShould.cs
+++ CorporateHotel.Tests/HotelManagement/Application/HotelServiceShould.cs
@@ -36,4 +36,20 @@ public class HotelServiceShould
         
         hotelRepository.Verify(repository => repository.FindHotelBy(hotelId), Times.Once);
     }
+
+    [Fact]
+    public void ReturnFoundHotelFromRepository()
+    {
+        var hotelRepository = new Mock<IHotelRepository>();
+        var hotelService = new HotelService(hotelRepository.Object);
+        var foundHotelName = "Found Hotel";
+        var foundHotelId = "600cfb4f-6337-41fa-8118-3069ce010305";
+        var hotelId = new HotelId(foundHotelId);
+        var foundHotel = new Hotel(hotelId, foundHotelName);
+        hotelRepository.Setup(repository => repository.FindHotelBy(hotelId)).Returns(foundHotel);
+
+        var returnHotel = hotelService.FindHotelBy(hotelId);
+        
+        Assert.Equal(foundHotel, returnHotel);
+    }
 }
