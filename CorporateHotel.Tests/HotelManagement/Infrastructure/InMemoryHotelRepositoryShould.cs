eef34e51189e19bfeb0b0dd3017c02d364bac73a
D-Arturo <arturomf88@gmail.com>
2024-08-10 16:31:23 +0200

    test: in memory repository

---
+++ CorporateHotel.Tests/HotelManagement/Infrastructure/InMemoryHotelRepositoryShould.cs
@@ -0,0 +1,29 @@
+﻿using CorporateHotel.HotelManagement.Domain;
+using CorporateHotel.HotelManagement.Infrastructure;
+using JetBrains.Annotations;
+
+namespace CorporateHotel.Tests.HotelManagement.Infrastructure;
+
+[TestSubject(typeof(InMemoryHotelRepository))]
+public class InMemoryHotelRepositoryShould
+{
+
+    [Fact]
+    public void AddNewHotel()
+    {
+        //Arrange
+        var newHotelId = "1a30df1f-b92d-4d7f-a875-ea295bd00138";
+        var newHotelName = "New Hotel";
+        var hotelId = new HotelId(newHotelId);
+        Hotel newHotel = new Hotel(hotelId, newHotelName);
+        IEnumerable<Hotel> hotels = new List<Hotel>();
+
+        var inMemoryHotelRepository = new InMemoryHotelRepository(hotels);
+        
+        //Act
+        inMemoryHotelRepository.AddHotel(newHotel);
+        
+        //Assert
+        Assert.Contains(newHotel, hotels);
+    }
+}
--- CorporateHotel/HotelManagement/Infrastructure/InMemoryHotelRepository.cs
+++ CorporateHotel/HotelManagement/Infrastructure/InMemoryHotelRepository.cs
@@ -4,6 +4,14 @@ namespace CorporateHotel.HotelManagement.Infrastructure;
 
 public class InMemoryHotelRepository : IHotelRepository
 {
+    public InMemoryHotelRepository(IEnumerable<Hotel> hotels)
+    {
+    }
+
+    public InMemoryHotelRepository()
+    {
+    }
+
     public void AddHotel(Hotel newHotel)
     {
         throw new NotImplementedException();
