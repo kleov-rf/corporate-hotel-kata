2abfdf07577f4e88d0a6b3766987a66aa1aba337
Kevin Ruiz <kleonardoruizflorez@gmail.com>
2024-08-10 14:58:18 +0200

    Initial commit

---
+++ CorporateHotel.Tests/Acceptance/HotelServiceShould.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CorporateHotel.Tests.Acceptance;
+
+public class HotelServiceShould
+{
+    [Fact]
+    public void AddNoExistingHotel()
+    {
+        // Arrange
+        string hotelName = "Name";
+        var hotelController = new HotelController();
+        
+        // Act
+        var addHotel = hotelController.AddHotel(hotelName);
+        
+        // Assert
+        Assert.IsType<OkObjectResult>(addHotel);
+
+        var foundHotelById = hotelController.FindHotelById(hotelID);
+        Assert.Equal(newHotel, foundHotelById);
+    }
+}
