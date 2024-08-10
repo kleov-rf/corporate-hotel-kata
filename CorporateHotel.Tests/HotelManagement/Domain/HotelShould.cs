7c87ef5b6a51b96dcd738407c79e0e57eefbca52
D-Arturo <arturomf88@gmail.com>
2024-08-10 18:36:55 +0200

    test: hotel self identifier

---
+++ CorporateHotel.Tests/HotelManagement/Domain/HotelShould.cs
@@ -0,0 +1,22 @@
+﻿using CorporateHotel.HotelManagement.Domain;
+using JetBrains.Annotations;
+
+namespace CorporateHotel.Tests.HotelManagement.Domain;
+
+[TestSubject(typeof(Hotel))]
+public class HotelShould
+{
+
+    [Fact]
+    public void IdentifyItselfByItsId()
+    {
+        var newHotelId = "88012242-b3f8-4a7c-8aed-52eb19209611";
+        HotelId hotelId = new HotelId(newHotelId);
+        var hotelName = "New Hotel";
+        Hotel hotel = new Hotel(hotelId, hotelName);
+
+        var isIdentifiedBy = hotel.IsIdentifiedBy(hotelId);
+        
+        Assert.True(isIdentifiedBy);
+    }
+}
